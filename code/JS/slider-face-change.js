ad026b17baca1d82ce210433f98f1a548304c4fe
PilibdaU <pilibdau@gmail.com>
2022-12-02 13:49:59 -0800

    fix reset button bug

--- code/JS/slider-face-change.js
+++ code/JS/slider-face-change.js
@@ -3,6 +3,8 @@ document.addEventListener("DOMContentLoaded", function() {
     const range = document.querySelector("#workLife");
     const faces = document.querySelectorAll(".form__question--2__faces > div");
 
+    const reset = document.querySelector(".button--reset");
+
     range.addEventListener('change', (event) => {
         const value = event.target.value;
 
@@ -13,5 +15,14 @@ document.addEventListener("DOMContentLoaded", function() {
         });
 
         faces[value-1].classList.add('active');
+    })
+
+    reset.addEventListener('click', () => {
+        faces.forEach((face) => {
+            if(face.classList.contains('active')){
+                face.classList.remove('active');
+            }
+        });
+        faces[2].classList.add('active');
     })
 })
